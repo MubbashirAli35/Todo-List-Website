480165f48493026b830e047dba6952f13bd12c8c
Mubbashir Ali <mubbashirali35@gmail.com>
2020-07-23 03:37:50 +0500

    Background added to Sign Up Page

--- Pages/SignUp/SignUp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default SignUp = (props) => {
-    return(
-        <div>
-            
-        </div>
-    );
-}
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+
+import './SignUp.css';
+
+export default function SignUp(props) {
+    return(
+        <Grid className='mainContainer' container max-width='xl'>
+            
+        </Grid>
+    );
+}
+
